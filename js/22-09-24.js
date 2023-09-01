8a66e9d8b8f55c8c035cded91be03b9c5b17068b
ttttkii913 <dbgkdud821@daum.net>
2023-09-01 12:46:29 +0900

    Back-up

---
+++ js/22-09-24.js
@@ -0,0 +1,5 @@
+function promf() {
+    let x = prompt("닉네임 설정");
+    let y = document.getElementById("name")
+    y.innerHTML = x;
+}
---
+++ js/nickname.js
@@ -0,0 +1,19 @@
+window.onload = function () { /*프롬프트 출력*/
+    let n = document.getElementById("button");
+    n.onclick = save;
+}
+
+function save()  {
+  alert('저장되었습니다');
+  document.location.href="main.html";
+}
+
+
+function over(obj){ /*마우스 핸들링 이벤트 */
+    obj.style.backgroundColor="rgb(193, 124, 235)";
+}
+function out(obj){ 
+    obj.style.backgroundColor="rgb(118, 22, 235)";
+}
+
+
